cad8fec88f31ac8ef266df711bdd9358257f3890
leo <leo42us@gmail.com>
2018-01-02 17:21:56 +0000

    start google oauth

--- react-redux/server/index.js
+++ react-redux/server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
+passport.use(new GoogleStrategy());
+
+const PORT = process.env.PORT || 9001;
+
 app.get('*', (req, res) => res.send('welcome back to express'));
 
-app.listen(9001, () => console.log('long time no Node'));
+app.listen(PORT, () => console.log('long time no Node'));
