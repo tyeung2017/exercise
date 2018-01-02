0392cd89344fcf3ca35544db8e13f9657bb5d9ca
leo <leo42us@gmail.com>
2018-01-02 18:06:39 +0000

    a bit more config for oauth

--- react-redux/server/index.js
+++ react-redux/server/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const { GoogleClientID, GoogleClientSecret } = require('../key');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy(
+  {
+    clientID: GoogleClientID,
+    clientSecret: GoogleClientSecret,
+    callbackURL: '/auth/google/callback',
+  },
+  (accessToken) => {
+    console.log(accessToken);
+  },
+));
 
 const PORT = process.env.PORT || 9001;
 
